ab8de2108202a4e1bb16cc2ffcf8865bd0cb99b5
Austin H <60478173+austinhogan11@users.noreply.github.com>
2021-01-05 15:12:29 -0500

    JSX React Basics

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var React = require("react");
+import React from "react";
+// var ReactDOM = require("react-dom");
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>Nicknames</h1>
+    <p>Chosen Soles</p>
+    <p>Cho</p>
+  </div>,
+  document.getElementById("root")
+);
